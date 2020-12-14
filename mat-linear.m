2de4f9f9166bb0c67df988a9cbbac9ee54f1b0e4
sinisaskoric1969 <sinisa.skoric1969@outlook.com>
2020-12-14 21:50:34 +0300

    plot

--- mat-linear.m
+++ mat-linear.m
@@ -1,6 +1,3 @@
-%% Question (a)
-
-% Matrix and initial value parameters
 A = [0.798 0.051; -0.715 1.088];
 xo = [1;0];
 
@@ -28,3 +25,28 @@ for k = 0:N
     x = A*xo;
     xo = x;
 end
+
+% We see that if we run N to a large number, then 'x' converges to zero.
+% The steady state is also asymptotically stable.
+%% Question (c)
+
+% Matrix and initial value parameters
+A = [0.798 0.051; -0.715 1.088];
+xo = [1;0];
+
+x0 = [1];
+x1 = [0];
+
+for k = 0:200
+    x = A*xo;
+    xo = x;
+    
+    % Store the two values 
+    x0 = [x0,x(1)];
+    x1 = [x1,x(2)];
+end
+
+plot(x0',x1','LineWidth',2.5)
+title('Phase plot')
+xlabel('x_{0}')
+ylabel('x_{1}')
